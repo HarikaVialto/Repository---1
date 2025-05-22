073772fae0f354fff5393a557983214b1cb6695b
Harika <harika.mula@vialto.com>
2025-05-22 16:44:25 +0530

    lettercombination

---
+++ Day4-pr.js
@@ -0,0 +1,29 @@
+"use strict";
+function letterCombinations(digits) {
+    if (!digits)
+        return [];
+    const digitToLetters = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+    const result = [];
+    function backtrack(index, currentCombination) {
+        if (index === digits.length) {
+            result.push(currentCombination);
+            return;
+        }
+        const letters = digitToLetters[digits[index]];
+        for (let letter of letters) {
+            backtrack(index + 1, currentCombination + letter);
+        }
+    }
+    backtrack(0, '');
+    return result;
+}
+console.log(letterCombinations("23"));
